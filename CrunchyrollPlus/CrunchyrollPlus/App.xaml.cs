e80d838ad73001e3eb85d196b45e19ebb98127d7
Hugo Persson <hugopersson7@outlook.com>
2020-01-08 19:22:44 +0100

    Initial Commit

---
+++ CrunchyrollPlus/CrunchyrollPlus/App.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace CrunchyrollPlus
+{
+    public partial class App : Application
+    {
+        public static HttpClient crunchyClient;
+        public static string sessionId;
+        public App()
+        {
+
+            InitializeComponent();
+            crunchyClient = new HttpClient();
+            crunchyClient.BaseAddress = new Uri("https://api.crunchyroll.com");
+            StartSession();
+            MainPage = new MainPage();
+        }
+        public static string GetPath(string req, string data)
+        {
+            return $"/{req}.0.json?session_id={sessionId}{data}";
+        }
+        
+        public async void StartSession()
+        {
+            const string deviceType = "com.crunchyroll.windows.desktop";
+
+            string url = $"/start_session.0.json?access_token=LNDJgOit5yaRIWN&device_id={Guid.NewGuid()}&device_type={deviceType}";
+
+            HttpResponseMessage httpResponseMessage = await crunchyClient.PostAsync(url, null);
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                string result = await httpResponseMessage.Content.ReadAsStringAsync();
+                JObject jObject = JObject.Parse(result);
+                if ((bool)jObject["error"])
+                {
+                    Console.WriteLine("LOG: ERROR");
+                }
+                else
+                {
+                    JObject data = (JObject)jObject["data"];
+                    sessionId = (string)data["session_id"];
+                    Console.WriteLine("LOG: done");
+                }
+
+
+            }
+        }
+        protected override void OnStart()
+        {
+
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CrunchyrollPlus/CrunchyrollPlus/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+
+namespace CrunchyrollPlus
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void Login(object sender, EventArgs args)
+        {
+            Console.WriteLine("login");
+        }
+        async void SendLoginRequest()
+        {
+            string username = this.username.Text;
+            string password = this.password.Text;
+
+            HttpResponseMessage httpResponseMessage = await App.crunchyClient.PostAsync(App.GetPath("login", $"&account={username}&password={password}"), null);
+        }
+    }
+}
