7f094dfcf4638710a7d8b1e82a2e9eb78010805a
Hugo Persson <hugper0119@skynet.halmstad.se>
2020-01-10 19:10:54 +0100

    Started integrating homescreen

--- CrunchyrollPlus/CrunchyrollPlus/Login.xaml.cs
+++ CrunchyrollPlus/CrunchyrollPlus/Login.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace CrunchyrollPlus
 {
@@ -26,6 +27,24 @@ namespace CrunchyrollPlus
             string password = this.password.Text;
 
             HttpResponseMessage httpResponseMessage = await App.crunchyClient.PostAsync(App.GetPath("login", $"&account={username}&password={password}"), null);
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                string res = await httpResponseMessage.Content.ReadAsStringAsync();
+                JObject o = JObject.Parse(res);
+                if ((bool)o["error"])
+                {
+
+                }
+                else
+                {
+                    JObject data = (JObject ) o["data"];
+                    JObject user = (JObject)data["user"];
+                    Application.Current.Properties["auth"] = data["auth"];
+                    Application.Current.Properties["loginExpire"] = data["expires"];
+                    await Navigation.PushAsync(new Homepage());
+
+                }
+            }
         }
     }
 }
