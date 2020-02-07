d455510d5d9d2429b8e5779032f58369e1ee7a64
Hugo Persson <hugopersson7@outlook.com>
2020-02-07 10:13:14 +0100

    Implemented toast

---
+++ CrunchyrollPlus/CrunchyrollPlus.Android/Services/ToastService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+[assembly: Xamarin.Forms.Dependency(typeof(CrunchyrollPlus.Droid.ToastService))]
+namespace CrunchyrollPlus.Droid
+{
+    
+    public class ToastService : IToastService
+    {
+        public void ShowToastShort(string message)
+        {
+            
+            Toast.MakeText(Application.Context, message, ToastLength.Short).Show();
+        }
+        public void ShowToastLong(string message)
+        {
+            
+
+            Toast.MakeText(Application.Context, message, ToastLength.Long).Show();
+        }
+
+    }
+}
